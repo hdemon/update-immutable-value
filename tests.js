9942e007b7225e82c7749d813c6c669ec3f7149c
Masami Yonehara @hdemon <zeitdiebe@gmail.com>
2017-10-03 19:43:21 +0900

    Add a basement of test cases

---
+++ tests.js
@@ -0,0 +1,13 @@
+const assert = require('power-assert');
+
+describe('update', () => {
+  context('when a type of the second argument is object' , () => {
+    context('and when it overlaps the original object inputed as the first argument' , () => {
+      it('should return object', () => {
+      });
+
+      it('shouldn\'t alter the original object', () => {
+      });
+    });
+  });
+});
